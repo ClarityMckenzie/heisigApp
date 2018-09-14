13aaf13987885b85da101098f3ad4406fbc0968b
Clarity <claritymckenzie@gmail.com>
2018-09-14 15:34:47 -0700

    adding oauth

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var ref = firebase.database().ref();
 
-// set-up Express and enable bodyParser
+// set-up request, Express, and enable bodyParser, FS too
+const request = require('request');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -18,10 +19,44 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(bodyParser.json());
+const fs = require('fs');
+
+// NodeJS encryption with CTR
+var crypto = require('crypto');
+var algorithm = process.env.CRYPTO_ALGORITHM;
+var password = process.env.CRYPTO_PASSWORD;
+
+
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
+
+// use fs to add user data to csv file, create one if it doesn't exist
+
+var userDetails = {"workspaceID": "", "userID": "", "token":
+""};;
+
+function newInstallation(detailsToSave) {
+
+fs.appendFile('auth.csv', detailsToSave, () => {
+  console.log('New installation!');
+
+});
+}
 
 // set-up response based on user input
 
-app.post('/', (req, res) => {
+app.post('/heisig', function(req, res) {
 
   var input = req.body.text;
   var firstChar = input.charAt(0);
@@ -30,7 +65,12 @@ app.post('/', (req, res) => {
   var keywordCollection = [];
   var kanjiCollection = [];
 
-  if (input > 0 && input < 2201) {
+  if (!input) {
+
+    res.send("What are we searching for?")
+
+
+  } else if (input > 0 && input < 2201) {
 
     var thisKanji = database.ref(input - 1);
 
@@ -112,12 +152,42 @@ app.post('/', (req, res) => {
 
 
       }, 200);
-
     })
-
   };
+});
+
+// authorize the app for other workspaces
+
+app.get('/authorize', function(req, res){
+
+var apiURI = 'https://slack.com/api/oauth.access?code=' + req.query.code +   "&client_id=" + process.env.CLIENT_ID + "&client_secret=" + process.env.CLIENT_SECRET + "&redirect_uri=" + process.env.REDIRECT_URI;
+
+request.post(apiURI, function(error, response, body){
 
 
+    if (!error && response.statusCode == 200) {
+
+      var oauthToken = JSON.parse(body).access_token;
+      var workspaceID = JSON.parse(body).team_id;
+      var userID = JSON.parse(body).user_id;
+
+      var encryptedToken = encrypt(oauthToken)
+      var encryptedWorkspaceID = encrypt(workspaceID)
+      var encryptedUserID = encrypt(userID);
+
+      userDetails.workspaceID = encryptedWorkspaceID;
+      userDetails.token = encryptedToken;
+      userDetails.userID = encryptedUserID;
+
+      var userDetailsString = JSON.stringify(userDetails)
+
+      newInstallation(userDetailsString)
+
+      res.redirect('https://www.hakoneprojects.com');
+
+    }
+
+  })
 });
 
 // start server
